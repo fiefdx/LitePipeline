4e80d5f41bf2a41b2fc2949f48369f403e3538e6
fiefdx <fiefdx@163.com>
2019-12-24 21:44:00 +0800

    construct StreamBaseHandler

--- manager/handlers/application.py
+++ manager/handlers/application.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import io
-import re
-import json
 import time
 import logging
 import shutil
@@ -12,7 +9,7 @@ import requests
 from tornado import web
 from tornado import gen
 
-from handlers.base import BaseHandler, BaseSocketHandler
+from handlers.base import BaseHandler, StreamBaseHandler
 from models.applications import ApplicationsDB
 from utils.listener import Connection
 from utils.common import file_sha1sum, file_md5sum, Errors
@@ -21,89 +18,7 @@ from config import CONFIG
 LOG = logging.getLogger("__name__")
 
 
-@web.stream_request_body
-class DeployApplicationHandler(BaseHandler):
-    PARSE_READY = 0
-    PARSE_FILE_PENDING = 1
-
-    def check_xsrf_cookie(self): # ignore xsrf check
-        pass
-
-    def prepare(self):
-        self.mimetype = self.request.headers.get("Content-Type")
-        self.boundary = "--%s" % (self.mimetype[self.mimetype.find("boundary")+9:])
-        self.boundary = self.boundary.encode("utf-8")
-        self.state = DeployApplicationHandler.PARSE_READY
-        self.output = None
-        self.find_filename = re.compile(b'filename="(.*)"')
-        self.find_mimetype = re.compile(b'Content-Type: (.*)')
-        self.find_field = re.compile(b'name="(.*)"')
-        self.start = time.time()
-        self.file_name = ""
-        self.file_path = ""
-        self.form_arguments = {}
-
-    def data_received(self, data):
-        LOG.debug("chunk size: %s", len(data))
-        try:
-            buff = data.split(self.boundary)
-            for index, part in enumerate(buff):
-                if part:
-                    if part == b"--\r\n":
-                        break
-                    if self.state == DeployApplicationHandler.PARSE_FILE_PENDING:
-                        if len(buff) > 1:
-                            self.output.write(part[:-2])
-                            self.output.close()
-                            self.state = DeployApplicationHandler.PARSE_READY
-                            continue
-                        else:
-                            self.output.write(part)
-                            self.output.flush()
-                            continue
-
-                    elif self.state == DeployApplicationHandler.PARSE_READY:
-                        stream = io.BytesIO(part)
-                        stream.readline()
-                        form_data_type_line = stream.readline()
-                        if form_data_type_line.find(b"filename") > -1:
-                            filename = re.search(self.find_filename, form_data_type_line).groups()[0]
-                            self.file_name = os.path.split(filename)[-1]
-                            self.file_path = os.path.join(CONFIG["data_path"].encode("utf-8"), b"tmp", self.file_name)
-                            self.output = open(self.file_path, "wb")
-                            content_type_line = stream.readline()
-                            mimetype = re.search(self.find_mimetype, content_type_line).groups()[0]
-                            LOG.debug("%s with %s" % (filename, mimetype.strip()))
-                            stream.readline()
-                            body = stream.read()
-                            if len(buff) > index + 1:
-                                self.output.write(body[:-2])
-                                self.output.flush()
-                                self.state = DeployApplicationHandler.PARSE_READY
-                            else:
-                                self.output.write(body)
-                                self.output.flush()
-                                self.state = DeployApplicationHandler.PARSE_FILE_PENDING
-                        else:
-                            stream.readline()
-                            form_name = re.search(self.find_field, form_data_type_line).groups()[0]
-                            form_value = stream.readline()
-                            if form_name:
-                                self.form_arguments[form_name.strip().decode("utf-8")] = form_value.strip().decode("utf-8")
-                            self.state = DeployApplicationHandler.PARSE_READY
-                            LOG.debug("%s = %s" % (form_name.strip(), form_value.strip()))
-        except Exception as e:
-            LOG.exception(e)
-
-    def get_form_argument(self, key, default_value):
-        result = default_value
-        try:
-            if key in self.form_arguments:
-                result = self.form_arguments[key]
-        except Exception as e:
-            LOG.exception(e)
-        return result
-
+class DeployApplicationHandler(StreamBaseHandler):
     @gen.coroutine
     def post(self):
         result = {"result": Errors.OK}
@@ -164,89 +79,7 @@ class DeleteApplicationHandler(BaseHandler):
         self.finish()
 
 
-@web.stream_request_body
-class UpdateApplicationHandler(BaseHandler):
-    PARSE_READY = 0
-    PARSE_FILE_PENDING = 1
-
-    def check_xsrf_cookie(self): # ignore xsrf check
-        pass
-
-    def prepare(self):
-        self.mimetype = self.request.headers.get("Content-Type")
-        self.boundary = "--%s" % (self.mimetype[self.mimetype.find("boundary")+9:])
-        self.boundary = self.boundary.encode("utf-8")
-        self.state = UpdateApplicationHandler.PARSE_READY
-        self.output = None
-        self.find_filename = re.compile(b'filename="(.*)"')
-        self.find_mimetype = re.compile(b'Content-Type: (.*)')
-        self.find_field = re.compile(b'name="(.*)"')
-        self.start = time.time()
-        self.file_name = ""
-        self.file_path = ""
-        self.form_arguments = {}
-
-    def data_received(self, data):
-        LOG.debug("chunk size: %s", len(data))
-        try:
-            buff = data.split(self.boundary)
-            for index, part in enumerate(buff):
-                if part:
-                    if part == b"--\r\n":
-                        break
-                    if self.state == UpdateApplicationHandler.PARSE_FILE_PENDING:
-                        if len(buff) > 1:
-                            self.output.write(part[:-2])
-                            self.output.close()
-                            self.state = UpdateApplicationHandler.PARSE_READY
-                            continue
-                        else:
-                            self.output.write(part)
-                            self.output.flush()
-                            continue
-
-                    elif self.state == UpdateApplicationHandler.PARSE_READY:
-                        stream = io.BytesIO(part)
-                        stream.readline()
-                        form_data_type_line = stream.readline()
-                        if form_data_type_line.find(b"filename") > -1:
-                            filename = re.search(self.find_filename, form_data_type_line).groups()[0]
-                            self.file_name = os.path.split(filename)[-1]
-                            self.file_path = os.path.join(CONFIG["data_path"].encode("utf-8"), b"tmp", self.file_name)
-                            self.output = open(self.file_path, "wb")
-                            content_type_line = stream.readline()
-                            mimetype = re.search(self.find_mimetype, content_type_line).groups()[0]
-                            LOG.debug("%s with %s" % (filename, mimetype.strip()))
-                            stream.readline()
-                            body = stream.read()
-                            if len(buff) > index + 1:
-                                self.output.write(body[:-2])
-                                self.output.flush()
-                                self.state = UpdateApplicationHandler.PARSE_READY
-                            else:
-                                self.output.write(body)
-                                self.output.flush()
-                                self.state = UpdateApplicationHandler.PARSE_FILE_PENDING
-                        else:
-                            stream.readline()
-                            form_name = re.search(self.find_field, form_data_type_line).groups()[0]
-                            form_value = stream.readline()
-                            if form_name:
-                                self.form_arguments[form_name.strip().decode("utf-8")] = form_value.strip().decode("utf-8")
-                            self.state = UpdateApplicationHandler.PARSE_READY
-                            LOG.debug("%s = %s" % (form_name.strip(), form_value.strip()))
-        except Exception as e:
-            LOG.exception(e)
-
-    def get_form_argument(self, key, default_value):
-        result = default_value
-        try:
-            if key in self.form_arguments:
-                result = self.form_arguments[key]
-        except Exception as e:
-            LOG.exception(e)
-        return result
-
+class UpdateApplicationHandler(StreamBaseHandler):
     @gen.coroutine
     def put(self):
         result = {"result": Errors.OK}
