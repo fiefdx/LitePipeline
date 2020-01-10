6fbdfca8ef8dff00e282bff43ef647b8617a81bc
fiefdx <fiefdx@163.com>
2020-01-10 16:24:40 +0800

    update multiple_actions example

--- examples/mulitple_actions/third.py
+++ examples/mulitple_actions/third.py
@@ -9,6 +9,7 @@ import datetime
 from pathlib import Path
 
 import tornado
+from litepipeline.models.action import Action
 
 import logger
 
@@ -18,40 +19,32 @@ home = str(Path.home())
 
 
 if __name__ == "__main__":
-    print("argv: %s" % sys.argv)
-    if len(sys.argv) < 2:
-        print("need workspace argument")
-    else:
-        workspace = sys.argv[1]
-        logs_directory = os.path.join(workspace, "logs")
-        fp = open(os.path.join(workspace, "input.data"), "r")
-        input_data = json.loads(fp.read())
-        fp.close()
+    workspace, input_data = Action.get_input()
 
-        logger.config_logging(file_name = "third.log",
-                              log_level = "DEBUG",
-                              dir_name = logs_directory,
-                              day_rotate = False,
-                              when = "D",
-                              interval = 1,
-                              max_size = 20,
-                              backup_count = 5,
-                              console = False)
-        LOG.debug("test start")
-        LOG.debug("input_data: %s", input_data)
+    logs_directory = os.path.join(workspace, "logs")
+    logger.config_logging(file_name = "third.log",
+                          log_level = "DEBUG",
+                          dir_name = logs_directory,
+                          day_rotate = False,
+                          when = "D",
+                          interval = 1,
+                          max_size = 20,
+                          backup_count = 5,
+                          console = False)
+    LOG.debug("test start")
+    LOG.debug("input_data: %s", input_data)
 
-        fp = open(os.path.join(workspace, "output.data"), "w")
-        data = {"messages": []}
-        for message in input_data["first"]["messages"]:
-            data["messages"].append(message)
-        for message in input_data["second"]["messages"]:
-            data["messages"].append(message)
-        for i in range(20, 30):
-            now = datetime.datetime.now()
-            message = "%s: hello world, tornado(%03d): %s" % (now, i, tornado.version)
-            data["messages"].append(message)
-            LOG.debug(message)
-            time.sleep(1)
-        fp.write(json.dumps(data, indent = 4))
-        fp.close()
-        LOG.debug("test end")
+    data = {"messages": []}
+    for message in input_data["first"]["messages"]:
+        data["messages"].append(message)
+    for message in input_data["second"]["messages"]:
+        data["messages"].append(message)
+    for i in range(20, 30):
+        now = datetime.datetime.now()
+        message = "%s: hello world, tornado(%03d): %s" % (now, i, tornado.version)
+        data["messages"].append(message)
+        LOG.debug(message)
+        time.sleep(1)
+
+    Action.set_output(data = data)
+    LOG.debug("test end")
