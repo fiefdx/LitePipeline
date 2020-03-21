ef79630a709ee5f193a3bd1e6fff213af9b30938
fiefdx <fiefdx@163.com>
2020-03-21 23:19:24 +0800

    manager & node enable CORS requests

--- litepipeline/litepipeline/manager/handlers/base.py
+++ litepipeline/litepipeline/manager/handlers/base.py
@@ -30,6 +30,13 @@ class BaseHandler(web.RequestHandler):
 
     def set_default_headers(self):
         self.set_header("Content-Type", 'application/json')
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', ' PUT, DELETE, OPTIONS')
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
 
     def get_user_locale(self):
         pass
