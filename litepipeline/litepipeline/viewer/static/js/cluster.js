c80158a99d741e31e0d671cc643463b5709f43fc
fiefdx <fiefdx@163.com>
2020-03-24 18:41:09 +0800

    update cluster page code

--- litepipeline/litepipeline/viewer/static/js/cluster.js
+++ litepipeline/litepipeline/viewer/static/js/cluster.js
@@ -76,10 +76,6 @@ function clusterInit (manager_host) {
         });
     }
 
-    function getHeaderTR(id, title, value) {
-        return '<th id="' + id + '" title="' + title + '"><div class="outer"><div class="inner">&nbsp;' + value + '</div></div></th>';
-    }
-
     function refreshNodeInfo(event) {
         var node_id = event.data.node_id;
         getClusterInfo(node_id);
@@ -129,47 +125,5 @@ function clusterInit (manager_host) {
             $('th#node_id').css("width", width + "%");
             $('td#node_id').css("width", width + "%");
         }
-    }
-
-    function is_in(v, l) {
-        for (var i=0; i<l.length; i++) {
-            if (v == l[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function hasVerticalScrollBar(el) {
-        var result = el.scrollHeight > el.clientHeight;
-        return result;
-    }
-
-    function getBrowserScrollSize() {
-        var css = {
-            "border":  "none",
-            "height":  "200px",
-            "margin":  "0",
-            "padding": "0",
-            "width":   "200px"
-        };
-
-        var inner = $("<div>").css($.extend({}, css));
-        var outer = $("<div>").css($.extend({
-            "left":       "-1000px",
-            "overflow":   "scroll",
-            "position":   "absolute",
-            "top":        "-1000px"
-        }, css)).append(inner).appendTo("body")
-        .scrollLeft(1000)
-        .scrollTop(1000);
-
-        var scrollSize = {
-            "height": (outer.offset().top - inner.offset().top) || 0,
-            "width": (outer.offset().left - inner.offset().left) || 0
-        };
-
-        outer.remove();
-        return scrollSize;
     }
 }
