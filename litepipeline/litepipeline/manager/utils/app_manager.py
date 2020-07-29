b706d466fdab98273820749606805ff9bf87fb24
fiefdx <fiefdx@163.com>
2020-07-29 18:49:19 +0800

    use app_path method to generate app path

--- litepipeline/litepipeline/manager/utils/app_manager.py
+++ litepipeline/litepipeline/manager/utils/app_manager.py
@@ -51,11 +51,14 @@ class AppLocalTarGzManager(AppManagerBase):
     def instance(cls):
         return cls._instance
 
+    def app_path(self, app_id):
+        return os.path.join(self.root_path, "applications", app_id[:2], app_id[2:4], app_id)
+
     def create(self, name, description, source_path):
         sha1 = file_sha1sum(source_path)
         LOG.debug("sha1: %s, %s", sha1, type(sha1))
         app_id = Applications.instance().add(name, sha1, description = description)
-        app_path = os.path.join(self.root_path, "applications", app_id[:2], app_id[2:4], app_id)
+        app_path = self.app_path(app_id)
         if os.path.exists(app_path):
             shutil.rmtree(app_path)
         os.makedirs(app_path)
@@ -84,7 +87,7 @@ class AppLocalTarGzManager(AppManagerBase):
                 sha1 = file_sha1sum(source_path)
                 data["sha1"] = sha1
                 LOG.debug("sha1: %s, %s", sha1, type(sha1))
-                app_path = os.path.join(self.root_path, "applications", app_id[:2], app_id[2:4], app_id)
+                app_path = self.app_path(app_id)
                 if os.path.exists(app_path):
                     shutil.rmtree(app_path)
                 os.makedirs(app_path)
@@ -116,7 +119,7 @@ class AppLocalTarGzManager(AppManagerBase):
         try:
             success = Applications.instance().delete(app_id)
             if success:
-                app_path = os.path.join(self.root_path, "applications", app_id[:2], app_id[2:4], app_id)
+                app_path = self.app_path(app_id)
                 if os.path.exists(app_path):
                     shutil.rmtree(app_path)
                     LOG.debug("remove directory: %s", app_path)
@@ -128,7 +131,7 @@ class AppLocalTarGzManager(AppManagerBase):
     def open(self, app_id, version = None):
         result = False
         try:
-            app_path = os.path.join(self.root_path, "applications", app_id[:2], app_id[2:4], app_id, "app.tar.gz")
+            app_path = self.app_path(app_id)
             if os.path.exists(app_path) and os.path.isfile(app_path):
                 result = open(app_path, "rb")
         except Exception as e:
