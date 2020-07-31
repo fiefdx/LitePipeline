c5f3b3443947b30cee2e6931cdb3c88d14aa2e1e
fiefdx <fiefdx@163.com>
2020-07-31 22:57:13 +0800

    viewer application info page basic layout finish

--- litepipeline/litepipeline/viewer/static/js/application_info.js
+++ litepipeline/litepipeline/viewer/static/js/application_info.js
@@ -1,3 +1,30 @@
-function applicationInfoInit (manager_host, app_id) {
-    console.log(manager_host, app_id);
+function applicationInfoInit (manager_host, application_id) {
+    var $btn_update = $('#btn-update');
+    var $btn_download = $('#btn-download');
+    var $btn_delete = $('#btn-delete');
+    var $btn_refresh = $('#btn-refresh');
+
+    getAppInfo();
+
+    function getAppInfo() {
+        var url = "http://" + manager_host + "/app/info?app_id=" + application_id + "&config=true";
+        $.ajax({
+            dataType: "json",
+            url: url,
+            success: function(data) {
+                if (data.result != "ok") {
+                    showWarningToast("operation failed", data.message);
+                } else {
+                    document.getElementById("app-info-json").textContent = JSON.stringify(data.app_info, undefined, 4);
+                    document.getElementById("app-config-json").textContent = JSON.stringify(data.app_config, undefined, 4);
+                }
+
+                hideWaitScreen();
+            },
+            error: function() {
+                showWarningToast("error", "request service failed");
+                hideWaitScreen();
+            }
+        });
+    }
 }
