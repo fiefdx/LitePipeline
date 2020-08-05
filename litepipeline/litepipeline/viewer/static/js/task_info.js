749c0cb602bc2bf975e84d8ba678f95d66a5f796
fiefdx <fiefdx@163.com>
2020-08-05 21:25:33 +0800

    task info page, support rerun, recover, stop task

--- litepipeline/litepipeline/viewer/static/js/task_info.js
+++ litepipeline/litepipeline/viewer/static/js/task_info.js
@@ -1,18 +1,27 @@
 function TaskInfoInit (manager_host, task_id) {
     var $btn_rerun = $('#btn-rerun');
+    var $btn_task_rerun = $('#btn-task-rerun');
     var $btn_recover = $('#btn-recover');
+    var $btn_task_recover = $('#btn-task-recover');
     var $btn_stop = $('#btn-stop');
+    var $btn_task_stop = $('#btn-task-stop');
     var $btn_download = $('#btn-download');
     var $btn_task_download = $('#btn-task-download');
     var $btn_refresh = $('#btn-refresh');
     var task_info = {};
 
     getTaskInfo();
+    $btn_rerun.bind('click', showTaskRerun);
+    $btn_task_rerun.bind('click', rerunTask);
+    $btn_recover.bind('click', showTaskRecover);
+    $btn_task_recover.bind('click', recoverTask);
+    $btn_stop.bind('click', showTaskStop);
+    $btn_task_stop.bind('click', stopTask);
     $btn_download.bind('click', showTaskDownload);
     $btn_task_download.bind('click', downloadTask);
     $btn_refresh.bind('click', refreshPage);
 
-    function getTaskInfo(with_history) {
+    function getTaskInfo() {
         var url = "http://" + manager_host + "/task/info?task_id=" + task_id;
         $.ajax({
             dataType: "json",
@@ -23,9 +32,11 @@ function TaskInfoInit (manager_host, task_id) {
                 } else {
                     task_info = data.task_info;
                     document.getElementById("task-info-json").textContent = JSON.stringify(data.task_info, undefined, 4);
+                    var task_running_info = '';
                     if (data.task_running_info) {
-                        document.getElementById("task-running-info-json").textContent = JSON.stringify(data.task_running_info, undefined, 4);
+                        task_running_info = JSON.stringify(data.task_running_info, undefined, 4);
                     }
+                    document.getElementById("task-running-info-json").textContent = task_running_info;
                 }
 
                 hideWaitScreen();
@@ -35,6 +46,90 @@ function TaskInfoInit (manager_host, task_id) {
                 showWarningToast("error", "request service failed");
                 hideWaitScreen();
                 $btn_refresh.removeAttr("disabled");
+            }
+        });
+    }
+
+    function showTaskRerun() {
+        $('#task-rerun-modal').modal('show');
+    }
+
+    async function rerunTask() {
+        var data = {"task_id": task_id};
+        $('#task-rerun-modal').modal('hide');
+        showWaitScreen();
+        await sleep(1000);
+        $.ajax({
+            type: "PUT",
+            url: "http://" + manager_host + "/task/rerun",
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                if (data.result != "ok") {
+                    showWarningToast("operation failed", data.message);
+                }
+                getTaskInfo();
+            },
+            error: function() {
+                showWarningToast("error", "request service failed");
+            }
+        });
+    }
+
+    function showTaskRecover() {
+        $('#task-recover-modal').modal('show');
+    }
+
+    async function recoverTask() {
+        var data = {"task_id": task_id};
+        $('#task-recover-modal').modal('hide');
+        showWaitScreen();
+        await sleep(1000);
+        $.ajax({
+            type: "PUT",
+            url: "http://" + manager_host + "/task/recover",
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                if (data.result != "ok") {
+                    showWarningToast("operation failed", data.message);
+                }
+                getTaskInfo();
+            },
+            error: function() {
+                showWarningToast("error", "request service failed");
+            }
+        });
+    }
+
+    function showTaskStop() {
+        $('#task-stop-modal').modal('show');
+    }
+
+    async function stopTask() {
+        var data = {"task_id": task_id, "signal": -9};
+        $('#task-stop-modal').modal('hide');
+        showWaitScreen();
+        await sleep(1000);
+        $.ajax({
+            type: "PUT",
+            url: "http://" + manager_host + "/task/stop",
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                if (data.result != "ok") {
+                    showWarningToast("operation failed", data.message);
+                }
+                getTaskInfo();
+            },
+            error: function() {
+                showWarningToast("error", "request service failed");
             }
         });
     }
